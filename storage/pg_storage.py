2576b0ffe12456dd01951a342dc4be066daa601a
AnatolyM <amisteke@gmail.com>
2026-04-14 00:56:13 +0300

    ef

--- storage/pg_storage.py
+++ storage/pg_storage.py
@@ -3,15 +3,19 @@ import os
 from scrapers.base import Article
 
 class PGStorage:
-    def __init__(self):
-        self.conn = psycopg2.connect(
-            host=os.getenv("DB_HOST", "db"),
-            port=os.getenv("DB_PORT", 5432),
-            dbname=os.getenv("DB_NAME", "news_db"),
-            user=os.getenv("DB_USER", "postgres"),
-            password=os.getenv("DB_PASSWORD", "_qg9_P__1WWpeffd"),
-        )
-        self._create_table()
+    def init(self):
+            db_url = os.getenv("DATABASE_URL")
+            if db_url:
+                self.conn = psycopg2.connect(db_url)
+            else:
+                self.conn = psycopg2.connect(
+                    host=os.getenv("DB_HOST", "db"),
+                    port=os.getenv("DB_PORT", "5432"),
+                    dbname=os.getenv("DB_NAME", "news_db"),
+                    user=os.getenv("DB_USER", "postgres"),
+                    password=os.getenv("DB_PASSWORD", "_qg9_P__1WWpeffd")
+                )
+            self._create_table()
 
     def _create_table(self):
         with self.conn.cursor() as cur:
