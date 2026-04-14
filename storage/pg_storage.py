d21086145cf702f622453ed8419a723c4428ee75
AnatolyM <amisteke@gmail.com>
2026-04-14 22:36:41 +0300

    time

--- storage/pg_storage.py
+++ storage/pg_storage.py
@@ -1,27 +1,39 @@
 import psycopg2
 import os
+import time
+
 from scrapers.base import Article
 
 class PGStorage:
     def init(self):
         self.conn = None
         db_url = os.getenv("DATABASE_URL")
-        try:
-            if db_url:
-                print(f"Connecting to DB via URL...")
-                self.conn = psycopg2.connect(db_url)
-            else:
-                print("Connecting to DB via components...")
-                self.conn = psycopg2.connect(
-                    host=os.getenv("DB_HOST", "db"),
-                    port=os.getenv("DB_PORT", "5432"),
-                    dbname=os.getenv("DB_NAME", "news_db"),
-                    user=os.getenv("DB_USER", "postgres"),
-                    password=os.getenv("DB_PASSWORD", "_qg9_P__1WWpeffd")
-                )
-            self._create_table()
-        except Exception as e:
-            print(f"FAILED TO CONNECT TO DB: {e}")
+        
+        retries = 5 # Пытаемся подключиться 5 раз
+        while retries > 0:
+            try:
+                if db_url:
+                    print(f"Connecting to DB via URL... ({retries} retries left)")
+                    self.conn = psycopg2.connect(db_url)
+                else:
+                    print(f"Connecting to DB via components... ({retries} retries left)")
+                    self.conn = psycopg2.connect(
+                        host=os.getenv("DB_HOST", "db"),
+                        port=os.getenv("DB_PORT", "5432"),
+                        dbname=os.getenv("DB_NAME", "news_db"),
+                        user=os.getenv("DB_USER", "postgres"),
+                        password=os.getenv("DB_PASSWORD", "_qg9_P__1WWpeffd")
+                    )
+                self._create_table()
+                print("Database initialized successfully!")
+                break # Если подключились — выходим из цикла
+            except Exception as e:
+                print(f"FAILED TO CONNECT TO DB: {e}. Retrying in 5s...")
+                time.sleep(5)
+                retries -= 1
+        
+        if not self.conn:
+            print("CRITICAL: Could not connect to database after retries.")
     def _create_table(self):
         with self.conn.cursor() as cur:
             cur.execute("""
