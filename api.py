cbdbcb77445b74ad4f8cdc106f0062c87773c4b3
AnatolyM <amisteke@gmail.com>
2026-04-15 00:42:00 +0300

    p

--- api.py
+++ api.py
@@ -49,7 +49,7 @@ def get_conn():
         port=os.getenv("DB_PORT", "5432"),
         dbname=os.getenv("DB_NAME", "news_db"),
         user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", "_qg9_P__1WWpeffd")
+        password=os.getenv("DB_PASSWORD", "qg9PlWWpeffd")
     )
 
 def init_sources_table():
