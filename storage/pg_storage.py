3cba67da7fc9553ad8cf57e4bdec1f82326f293a
AnatolyM <amisteke@gmail.com>
2026-04-15 00:45:43 +0300

    p23

--- storage/pg_storage.py
+++ storage/pg_storage.py
@@ -14,7 +14,7 @@ class PGStorage:
                     port=os.getenv("DB_PORT", "5432"), # Внутри сети Docker всегда 5432
                     dbname=os.getenv("DB_NAME", "news_db"),
                     user=os.getenv("DB_USER", "postgres"),
-                    password=os.getenv("DB_PASSWORD", "_qg9_P__1WWpeffd")
+                    password=os.getenv("DB_PASSWORD", "qg9PlWWpeffd")
                 )
                 self._create_table()
                 print("Database initialized successfully!")
