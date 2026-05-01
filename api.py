179951385a47b9952fcfd5f35c46beda5db3bcec
AnatolyM <amisteke@gmail.com>
2026-05-01 20:46:42 +0300

    Revert "fixGrade  articles_query=f""""
    This reverts commit 943338a990665daa8def0134081eed33178803a0.

--- api.py
+++ api.py
@@ -134,17 +134,9 @@ def get_articles(
         """
         cur.execute(count_query, params)
         total = cur.fetchone()["total"]
+
         articles_query = f"""
-            SELECT
-                id,
-                title,
-                url,
-                source,
-                description,
-                published,
-                collected_at,
-                primary_scale_id,
-                semantic_scales
+            SELECT *
             FROM articles
             WHERE {where_sql}
             ORDER BY published DESC NULLS LAST
